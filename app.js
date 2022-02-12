f75cb186d32346f8498160854516868959b8be3d
Bryan Abi Karam <bryancrisso@outlook.com>
2022-02-12 11:36:28 +0000

    Added ability to download image
    also included reimg.js and require.js

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const   cameraView = document.querySelector("#camera--view"),
         cameraOutput = document.querySelector("#camera--output"),
         cameraSensor = document.querySelector("#camera--sensor"),
         cameraTrigger = document.querySelector("#camera--trigger");
+        downloadTrigger = document.querySelector("#download--image");
+
+let downloadCount = 0;
 
 function cameraStart()
 {
@@ -24,7 +27,16 @@ cameraTrigger.onclick = () =>
     cameraSensor.width = cameraView.videoWidth;
     cameraSensor.height = cameraView.videoHeight;
     cameraSensor.getContext("2d").drawImage(cameraView,0,0);
-    cameraOutput.src = cameraSensor.toDataURL("image/webp");
+    let imageOutput = ReImg.fromCanvas(cameraSensor);
+    cameraOutput.src = imageOutput.toImg().src;
+    downloadTrigger.style.display = "inline";
+    downloadTrigger.onclick = () =>
+    {
+        imageOutput.downloadPng("tattoo" + downloadCount);
+        downloadCount++;
+    }
+
+    //cameraOutput.src = cameraSensor.toDataURL("image/webp");
     cameraOutput.classList.add("taken");
 };
 
