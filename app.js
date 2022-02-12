f9d7908358b6235b6e21970e7c36ee0b6a6726d1
Bryan Abi Karam <bryancrisso@outlook.com>
2022-02-12 15:03:33 +0000

    Added cancel and flip buttons
    also some UI changes

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ const   cameraView = document.querySelector("#camera--view"),
         cameraSensor = document.querySelector("#camera--sensor"),
         cameraTrigger = document.querySelector("#camera--trigger");
         downloadTrigger = document.querySelector("#download--image");
+        cancelTrigger = document.querySelector("#cancel--image");
+        flipCamera = document.querySelector("#flip--cam");
 
 let downloadCount = 0;
+const delay = async ms => new Promise(res => setTimeout(res, ms));
 
 function cameraStart()
 {
@@ -20,15 +23,20 @@ function cameraStart()
         {
             console.error("oops, something broke", error);
         });
+    //hide the canvas
+    cameraSensor.style.display="none";
 }
 
-cameraTrigger.onclick = () => 
+cameraTrigger.onclick = async () => 
 {
+    cameraOutput.classList.remove("taken");
+
     cameraSensor.width = cameraView.videoWidth;
     cameraSensor.height = cameraView.videoHeight;
     cameraSensor.getContext("2d").drawImage(cameraView,0,0);
     let imageOutput = ReImg.fromCanvas(cameraSensor);
     cameraOutput.src = imageOutput.toImg().src;
+    
     downloadTrigger.style.display = "inline";
     downloadTrigger.onclick = () =>
     {
@@ -36,8 +44,37 @@ cameraTrigger.onclick = () =>
         downloadCount++;
     }
 
+    cancelTrigger.style.display = "inline";
+    cancelTrigger.onclick = () =>
+    {
+        downloadTrigger.style.display = "none";
+        cancelTrigger.style.display = "none";
+        cameraOutput.classList.remove("taken");
+        cameraOutput.src = "transparency.png";
+    }
+    //wait for capture animation to reset
+    await delay(100);
+
     //cameraOutput.src = cameraSensor.toDataURL("image/webp");
     cameraOutput.classList.add("taken");
 };
+
+flipCamera.onclick = () =>
+{
+    if (constraints.video.facingMode == "user")
+    {
+        constraints.video.facingMode = "environment";
+        cameraView.style.transform = "scaleX(1)";
+        cameraSensor.style.transform = "scaleX(1)";
+        cameraOutput.style.transform = "scaleX(1)";
+    }
+    else
+    {
+        constraints.video.facingMode = "user";
+        cameraView.style.transform = "scaleX(-1)";
+        cameraSensor.style.transform = "scaleX(-1)";
+        cameraOutput.style.transform = "scaleX(-1)";
+    }
+}
 
 window.addEventListener("load", cameraStart, false);
